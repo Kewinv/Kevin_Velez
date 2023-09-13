859097fe2695311666b226d3ca081356bfc397d1
kevin <kevin@KEWIN>
2023-09-13 16:22:11 -0500

    Agregar archivos de proyecto.

---
+++ MT 13.09.23/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MT130923
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido usuario, digita el numero");
+
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine ($"\nEl valor resultante del numero que diste es : {7 * (num / 2 )}");
+            }
+
+            else if (num % 2 == 1) 
+            {
+                Console.WriteLine($"\nEl valor resultante del numero que diste es : { -1 * (num - 1) }");
+            }
+
+
+        }
+    }
+}
